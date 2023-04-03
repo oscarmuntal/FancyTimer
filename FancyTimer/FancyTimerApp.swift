67ebdf90ee41a9adfaf686b8f41dbf968f55c54b
Òscar Muntal <oscar.muntal@gmail.com>
2023-04-03 19:44:59 +0200

    Initial Commit

---
+++ FancyTimer/FancyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancyTimerApp.swift
+//  FancyTimer
+//
+//  Created by Òscar Muntal on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FancyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
